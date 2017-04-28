0473c46174c4f2f1de2f3a44f6c0c0aea6aca0f8
APRENDIZ <domain1@domain1.com>
2017-04-28 10:54:19 -0500

    Versión3_Yesid

--- src/Time.java
+++ src/Time.java
@@ -1,3 +1,7 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +13,42 @@
  * @author APRENDIZ
  */
 public class Time {
+    
+    
+     public void alarma(int horas, int minutos) {
+
+        if (horas >= 1) {
+
+            int min = (horas * 60) - 1;
+            horas = min;
+
+            for (int hora = horas; hora >= 0; hora--) {
+
+                for (int minuto = minutos; minuto >= 0; minuto--) {
+                    stop();
+                    System.out.println(hora + ":" + minuto + " sec.");
+
+                    if (hora == 0 && minuto == 0) {
+                        System.out.println("Suena la alarma Levantate....");
+                        break;
+                    } else if (minuto == 0) {
+                        minuto = 59;
+                        hora--;
+                    }
+                }
+            }
+        } else {
+            System.out.println("Formato no permitido");
+        }
+    }
+
+    public void stop() {
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
